91a0093ef9bd62bb7c7d4e5bd56dd6c71187c968
ShreyasPakhare <shreyas_pp@ee.iitr.ac.in>
2021-09-11 01:30:59 +0530

    Added No Connection View

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.newsapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -25,8 +28,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String BASE_URL ="https://content.guardianapis.com/search?api-key=test";
-//    private static String BASE_URL = "https://content.guardianapis.com/search?q=aadsfndjfnkjdn&api-key=test";
+    private static String BASE_URL = "https://content.guardianapis.com/search?api-key=test";
+//    private static String BASE_URL = "https://content.guardianapis.com/search?q=aadsfndjfnkjdn&api-key=test"; //To check for Empty State View
 
     //Recyclerview object
     private RecyclerView recyclerView;
@@ -34,14 +37,21 @@ public class MainActivity extends AppCompatActivity {
     //the hero list where we will store all the hero objects after parsing json
     List<News> newsList;
 
+    //Relative Layout To store empty list view.
     RelativeLayout relativeLayout;
+
+    //Relative Layout To store No Internet Connection Layout.
+    RelativeLayout noconnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        relativeLayout  = findViewById(R.id.empty_stateview);
+        noconnection = findViewById(R.id.no_internet_view);
+        noconnection.setVisibility(View.GONE);
+
+        relativeLayout = findViewById(R.id.empty_stateview);
 
         relativeLayout.setVisibility(View.GONE);
 
@@ -62,80 +72,89 @@ public class MainActivity extends AppCompatActivity {
         loadNewsList();
 
 
-
-
     }
 
     private void loadNewsList() {
+
+        ConnectivityManager cm =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        if (!isConnected)
+            noconnection.setVisibility(View.VISIBLE);
+        else {
 //        //getting the progressbar
 //        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
 //
 //        //making the progressbar visible
 //        progressBar.setVisibility(View.VISIBLE);
 
-        //creating a string request to send request to the url
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, BASE_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+            //creating a string request to send request to the url
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, BASE_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
 //                        //hiding the progressbar after completion
 //                        progressBar.setVisibility(View.INVISIBLE);
 
 
-                        try {
-                            //getting the whole json object from the response
-                            JSONObject obj1 = new JSONObject(response);
+                            try {
+                                //getting the whole json object from the response
+                                JSONObject obj1 = new JSONObject(response);
 
-                            JSONObject obj = obj1.getJSONObject("response");
+                                JSONObject obj = obj1.getJSONObject("response");
 
-                            //we have the array named hero inside the object
-                            //so here we are getting that json array
-                            JSONArray newsArray = obj.getJSONArray("results");
+                                //we have the array named hero inside the object
+                                //so here we are getting that json array
+                                JSONArray newsArray = obj.getJSONArray("results");
 
-                            //now looping through all the elements of the json array
-                            for (int i = 0; i < newsArray.length(); i++) {
-                                //getting the json object of the particular index inside the array
-                                JSONObject newsObject = newsArray.getJSONObject(i);
+                                //now looping through all the elements of the json array
+                                for (int i = 0; i < newsArray.length(); i++) {
+                                    //getting the json object of the particular index inside the array
+                                    JSONObject newsObject = newsArray.getJSONObject(i);
 
-                                //creating a newsobject and giving them the values from json object
-                                News news = new News(newsObject.getString("webTitle"),
-                                        newsObject.getString("sectionName"),newsObject.getString("webUrl"));
+                                    //creating a newsobject and giving them the values from json object
+                                    News news = new News(newsObject.getString("webTitle"),
+                                            newsObject.getString("sectionName"), newsObject.getString("webUrl"));
 
-                                //adding the news to newslist
-                                newsList.add(news);
+                                    //adding the news to newslist
+                                    newsList.add(news);
+                                }
+
+                                //creating custom adapter object
+                                NewsAdapter adapter = new NewsAdapter(newsList, getApplicationContext());
+
+                                //adding the adapter to listview
+                                recyclerView.setAdapter(adapter);
+
+                                if (newsList.isEmpty()) {
+
+                                    relativeLayout.setVisibility(View.VISIBLE);
+                                    recyclerView.setVisibility(View.GONE);
+                                }
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-
-                            //creating custom adapter object
-                            NewsAdapter adapter = new NewsAdapter(newsList, getApplicationContext());
-
-                            //adding the adapter to listview
-                            recyclerView.setAdapter(adapter);
-
-                            if(newsList.isEmpty())
-                            {
-
-                                relativeLayout.setVisibility(View.VISIBLE);
-                                recyclerView.setVisibility(View.GONE);
-                            }
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
                         }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //displaying the error in toast if occurrs
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //displaying the error in toast if occurrs
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
-        //creating a request queue
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+            //creating a request queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        //adding the string request to request queue
-        requestQueue.add(stringRequest);
+            //adding the string request to request queue
+            requestQueue.add(stringRequest);
+        }
     }
 }
