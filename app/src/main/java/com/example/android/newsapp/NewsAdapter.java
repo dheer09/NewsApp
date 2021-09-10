d4e0c0a02be1f23886299cd0945fa4d9661bf8af
ShreyasPakhare <shreyas_pp@ee.iitr.ac.in>
2021-09-10 11:32:40 +0530

    Added ViewHolder Pattern

--- app/src/main/java/com/example/android/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -7,11 +7,15 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-
 import java.util.List;
 
 public class NewsAdapter extends ArrayAdapter<News> {
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView webTitle;
+        TextView sectionName;
+    }
 
     //the hero list that will be displayed
     private List<News> newsList;
@@ -24,27 +28,30 @@ public class NewsAdapter extends ArrayAdapter<News> {
         this.mCtx = context;
         this.newsList = news;
     }
-    //this method will return the list item
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //getting the layoutinflater
-        LayoutInflater inflater = LayoutInflater.from(mCtx);
-
-        //creating a view with our xml layout
-        View listViewItem = inflater.inflate(R.layout.list_item, null, true);
-
-        //getting text views
-        TextView webTitle = listViewItem.findViewById(R.id.webTitle);
-        TextView sectionName = listViewItem.findViewById(R.id.sectionName);
-
-        //Getting the hero for the specified position
-        News news = newsList.get(position);
-
-        //setting hero values to textviews
-        webTitle.setText(news.getWebTitle());
-         sectionName.setText(news.getSectionName());
-
-        //returning the listitem
-        return listViewItem;
+        // Get the data item for this position
+        News news = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+        if (convertView == null) {
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(mCtx);
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+            viewHolder.webTitle= (TextView) convertView.findViewById(R.id.webTitle);
+            viewHolder.sectionName = (TextView) convertView.findViewById(R.id.sectionName);
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+        viewHolder.sectionName.setText(news.getSectionName());
+        viewHolder.webTitle.setText(news.getWebTitle());
+        // Return the completed view to render on screen
+        return convertView;
     }
 }
